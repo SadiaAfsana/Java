c211451193ec57797150990590addf46512f2cf7
SadiaAfsana <flora24488@gmail.com>
2022-01-01 15:43:17 +0600

    Update

--- Prime_Number.java
+++ Prime_Number.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 public class Prime_Number {
     public static void main(String arg[]) {
         {
-            int num = 0, i = 2, j = 1;
+            int num = 0, i = 2;
             System.out.print("Enter n value: ");
             Scanner sc = new Scanner(System.in);
             int n = sc.nextInt();
             System.out.println("Prime numbers from 2 to " + n + " are ");
 
             while (i < n) {
-                j = 1;
-                int count = 0;
+                int j = 1, count = 0;
                 while (j <= i) {
-                    if (i % j == 0) count++;
+                    if (i % j == 0)
+                        count++;
                     j++;
                 }
                 if (count == 2) {

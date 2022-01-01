e23adbf1b97f5983aa197267282515af9dffb7fa
SadiaAfsana <flora24488@gmail.com>
2022-01-01 23:30:37 +0600

    Update

--- Cgpa_Grade.java
+++ Cgpa_Grade.java
@@ -7,24 +7,37 @@ public class Cgpa_Grade {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        double Cgpa = 0;
-        System.out.print("Enter No. of semesters: ");
+        int sumcch = 0;
+        double CGPA = 0, sumTCH = 0, TGPA = 0, sumTGPA = 0, cp = 0;
+
+        System.out.print("Enter No. of Semesters Completed: ");
         int s = in.nextInt();
 
-        for (int i = 1; i <= s; i++) {
-            System.out.print("Enter Semester" + i + " GPA: ");
-            double g1 = in.nextDouble();
-            Cgpa = Cgpa + g1;
+        for (int j = 1; j <= s; j++) {
+            System.out.print("Enter No. of Courses for semester" + j + ":");
+            int c = in.nextInt();
+            for (int i = 1; i <= c; i++) {
+                System.out.print("Enter Course" + i + " Total Credit Hours: ");
+                int cch = in.nextInt();
+                sumcch = sumcch + cch;
+                System.out.print("Enter Courser" + i + " Grade Point: ");
+                double cgp = in.nextDouble();
+                cp = cp + (cch * cgp);
+            }
+            TGPA = cp / sumcch;
+            System.out.println("Your Total GPA for semester  " + j + " is " + TGPA);
+            sumTGPA = sumTGPA + TGPA;
+            CGPA = sumTGPA / s;
         }
 
-        System.out.format("Your Cgpa is : %.2f", Cgpa / s);
+        System.out.format("Your Cgpa is : %.2f", CGPA);
         System.out.println("");
 
-        if (Cgpa >= 3.8)
+        if (CGPA >= 3.8)
             System.out.println("Excellent: Grade A");
-        else if (Cgpa < 3.79 && Cgpa >= 3.5)
+        else if (CGPA < 3.79 && CGPA >= 3.5)
             System.out.println("Good: Grade B");
-        else if (Cgpa < 3.49 && Cgpa >= 3.0)
+        else if (CGPA < 3.49 && CGPA >= 3.0)
             System.out.println("Poor: Grade C");
         else
             System.out.println("Failed!");

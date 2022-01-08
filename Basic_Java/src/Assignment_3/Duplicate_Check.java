bf2efc158397b265d75a9c5621239c18c620c69e
SadiaAfsana <flora24488@gmail.com>
2022-01-08 13:45:10 +0600

    Update

--- Basic_Java/src/Assignment_3/Duplicate_Check.java
+++ Basic_Java/src/Assignment_3/Duplicate_Check.java
@@ -1,11 +1,20 @@
 package Assignment_3;
 //2. Write a program to find if any element is duplicate. If duplicate, show the value.
 
+import java.util.Scanner;
+
 public class Duplicate_Check {
 
     public static void main(String[] args) {
-        int[] myArray = {11, 5, 5, 9, 9};
-        dup(myArray);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of array elements : ");
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+        dup(array);
     }
 
     public static void dup(int[] array) {
@@ -13,7 +22,7 @@ public class Duplicate_Check {
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] == array[j]) {
-                    System.out.println(array[j]);
+                    System.out.println("Duplicate value: " + array[j]);
                     isExist = true;
                 }
             }
